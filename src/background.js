aa4b78736dbcd330e82d4a5b9a76d4380ff37397
Altan Aldeniz <altanaldeniz@gmail.com>
2024-02-13 20:54:51 +0300

    remove most of the permissions

--- src/background.js
+++ src/background.js
@@ -7,20 +7,3 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
     })
   }
 })
-
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (
-    !tab.url.startsWith('chrome://') &&
-    !tab.url.startsWith('edge://') &&
-    !tab.url.startsWith('https://chromewebstore.google.com/')
-  ) {
-    const { active } = await chrome.storage.local.get()
-    if (active) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        files: ['content.js'],
-        injectImmediately: true,
-      })
-    }
-  }
-})
